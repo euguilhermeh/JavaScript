a563a06e5f0ce58c40759dc7d514799f9b83d139
Guilherme <108306976+euguilhermeh@users.noreply.github.com>
2023-12-05 19:40:16 -0300

    Add files via upload

---
+++ Função.js
@@ -0,0 +1,17 @@
+function MeuNome (nome){
+    console.log ('Meu nome é' + nome);
+}
+
+MeuNome(' xxxxx');
+
+
+
+function VerificarIdade (idade) {
+    if (idade > 17) {
+        console.log ('Sou maior de idade');
+    } else {
+        console.log ('Sou menor de idade');
+    }
+}
+
+VerificarIdade(16);
